71775949d42d733cb3407e8a5f1cd512291fd6d6
Tom Miller <thomas.j.c.miller@outlook.com>
2019-02-22 18:51:59 +0000

    add price to total

--- lib/till.rb
+++ lib/till.rb
@@ -4,12 +4,13 @@ class Till
   attr_reader :items
 
   def initialize (items = Items.new)
-    @items = items
-
+    @items = items,
+    @total = []
   end
 
   def checkout(order)
     price = items.menu.map {|key| key[(order)]}
+    @total.push(price.join)
     return order + " " + price.join
   end
 end
